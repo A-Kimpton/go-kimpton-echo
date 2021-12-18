0c11205bfc6a4f4b124364595dbaf2d98bf2487d
Antony Kimpton <antony@antonykimpton.com>
2021-12-18 15:13:10 +0000

    Added linux build file

---
+++ autobuild.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Builds the linux binaries
+
+# Setup env vars
+export GOARCH=amd64
+
+# Service name
+service="echoservice"
+rootdir=`pwd`
+
+echo "$service :: autobuild.sh"
+echo "GOBIN=$GOBIN"
+
+# Build the client binary
+echo
+echo "Building the client binary $service-client"
+cd $rootdir && cd client && go install $service-client.go
+echo "Client complete!"
+
+# Build the server binary
+echo
+echo "Building the server binary $service-server"
+cd $rootdir && cd server && go install $service-server.go
+echo "Server complete!"
+
+# Print finished message
+echo 
+echo "finished."
